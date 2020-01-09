359db692dc88a6ea54ee12dadecfde422ab1428d
Rafael Oliveira <rafaelbrk.so@gmail.com>
2020-01-09 10:11:47 -0300

    Modulo 1 de estudo REACT

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React, { Component, Fragment } from 'react';
+import { render } from "react-dom";
+import Button from "./Button";
+import "./style.scss";
+
+class App extends Component {
+    state = {
+        counter: 0
+    };
+
+    handleClick = () => {
+        this.setState({ counter: this.state.counter += 1 });
+    };
+    
+    render() {
+        return (
+            <Fragment>
+                <h1 className="title">hello rocketseat</h1>
+                <h2 style={{ color: '#f00' }}>{this.state.counter}</h2>
+                <Button onClick={this.handleClick}>Enviou</Button>
+            </Fragment> 
+        );
+    }
+}
+
+render(<App/>, document.getElementById("app"));
